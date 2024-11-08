07bf67a80db8d24afe53895546035050de56d421
Jack Rawlinson <jack.rawlinson@seven20.io>
2024-11-08 14:02:04 +0000

    Answer position are now ranodmised

--- Quiz_app.js
+++ Quiz_app.js
@@ -8,7 +8,6 @@ function loadfile(event){
     /*
     Load data in from a CSV file containing questions and answers
     */
-    document.getElementById("debug").innerHTML +="<br> creating FileReader";
     // FileReader object to handle reading in the data
     const reader = new FileReader();
     // Run load_event when file has been read, this will store the necessary data
@@ -23,30 +22,47 @@ function loadfile(event){
 function load_event(event){
     // Initialize a temp array
     let csv_data = []
-    document.getElementById("debug").innerHTML +="<br>" + event;
-    document.getElementById("debug").innerHTML +="<br>" + event.target.result.split(",");
     // Store text data in temp array 
     csv_data = (event.target.result.split(","));
-    document.getElementById("debug").innerHTML +="<br>" + csv_data[0];
     // Store questions and answers in quiz_data array
     for(let i=0;i<(csv_data.length/5); i++){
         quiz_data.push([csv_data[i*5], csv_data[(i*5)+1], csv_data[(i*5)+2], csv_data[(i*5)+3], csv_data[(i*5)+4]]);
     }
+    // Prevent user from escaping the scope of the array
     if(quiz_data.length == 1){document.getElementById("next_button").disbaled = true;}
     // Load first question 
     show_question();
 }
 
 function show_question(){
+    document.getElementById("debug").innerHTML = quiz_data[question_no];
+    random_sort(quiz_data[question_no]);
+    document.getElementById("debug").innerHTML += "<br>" + quiz_data[question_no];
     // Update elements in question_div to show the current question
-    document.getElementById("debug").innerHTML += "<br> quiz_data = " + quiz_data;
     document.getElementById("Question").innerHTML = quiz_data[question_no][0];
     document.getElementById("answer_1").textContent = quiz_data[question_no][1];
     document.getElementById("answer_2").textContent = quiz_data[question_no][2];
     document.getElementById("answer_3").textContent = quiz_data[question_no][3];
     document.getElementById("answer_4").textContent = quiz_data[question_no][4];
 }
+
+function random_sort(array){
+    let index = array.length - 2;
+    //document.getElementById("debug").innerHTML += "<br>" + index;
+    while(index > 0){
+        document.getElementById("debug").innerHTML += "<br>" + index;
+        let random_index = Math.floor(Math.random() * index) + 2;
+        document.getElementById("debug").innerHTML += " " + random_index;
+        index--;
+
+            // And swap it with the current element.
+        [array[index+1], array[random_index]] = [array[random_index], array[index+1]];
+    }
+}
 function next_question(){
+    /*
+    Move to next question and update next & back buttons to prevent user from going out of scope of array
+    */
     question_no += 1;
     document.getElementById("back_button").disabled = false;
      if(question_no == quiz_data.length-1){
@@ -55,6 +71,9 @@ function next_question(){
     show_question();
 }
 function previous_question(){
+    /*
+    Move to previous question and update next & back buttons to prevent user from going out of scope of array
+    */
     question_no -= 1;
     document.getElementById("next_button").disabled = false;
     if(question_no == 0){
