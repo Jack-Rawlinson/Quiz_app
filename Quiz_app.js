c5e20b25ad18f739e7c65c67bfaa19c6654a5af5
Jack Rawlinson <jack.rawlinson@seven20.io>
2024-11-08 16:16:25 +0000

    Answers are remembered and a score is now shown

--- Quiz_app.js
+++ Quiz_app.js
@@ -2,7 +2,9 @@
 document.getElementById("fileinput").addEventListener("change", loadfile);
 // Initialize global variables
 let quiz_data = [];
+let answer_order = [];
 let question_no = 0;
+let correct_answers = 0;
 
 function loadfile(event){
     /*
@@ -14,20 +16,18 @@ function loadfile(event){
     reader.onload = load_event;
     // Read data as a string
     reader.readAsText(event.target.files[0]);
-    document.getElementById("debug").innerHTML +="<br>Reader = " + typeof(data);
     // Hide div for entering files and reveal div for anwsering questions
     document.getElementById("file_div").style.display = "none";
     document.getElementById("question_div").style.display = "";
 }
 function load_event(event){
-    // Initialize a temp array
-    let csv_data = []
-    // Store text data in temp array 
-    csv_data = (event.target.result.split(","));
-    // Store questions and answers in quiz_data array
-    for(let i=0;i<(csv_data.length/5); i++){
-        quiz_data.push([csv_data[i*5], csv_data[(i*5)+1], csv_data[(i*5)+2], csv_data[(i*5)+3], csv_data[(i*5)+4]]);
-    }
+    // Split data by rows
+    let rows = event.target.result.split("\n");
+    // Store data in temp array to a 
+    let csv_data =[];
+    rows.forEach(row => csv_data.push(row.split(",")));
+    // Store questions and answers in quiz_data array, false variable tells if question has already been answered or not
+    csv_data.forEach(row => quiz_data.push([row[0], row[1], row[2], row[3], row[4], false]));
     // Prevent user from escaping the scope of the array
     if(quiz_data.length == 1){document.getElementById("next_button").disbaled = true;}
     // Load first question 
@@ -35,36 +35,69 @@ function load_event(event){
 }
 
 function show_question(){
-    // array to decide order that answers appear on buttons
-    let answer_index = [1,2,3,4];
-    // Randomize order
-    random_sort(answer_index);
+    document.getElementById("debug").innerHTML += "<br>" + quiz_data;
+    if(answer_order.length <= question_no){
+        // Array to decide order that answers appear on buttons
+        let answer_index = [1,2,3,4];    
+        document.getElementById("debug").innerHTML += "<br>answerindex = " + answer_index ;
+        // Randomize order
+        random_sort(answer_index);
+        // Push an element in to store index of button that is pressed
+        answer_index.push(0)
+        answer_order.push(answer_index);
+    }
+    document.getElementById("Question").textContent = quiz_data[question_no][0];
+    document.getElementById("score").textContent = correct_answers+ "/" + quiz_data.length;
     // Update elements in question_div to show the current question
-    for(i in answer_index){
+    for(let i=0; i<(answer_order[question_no].length - 1); i++){
         // variable to store index of answer (eaier to read this way)
-        let index = answer_index[i];
+        let index = answer_order[question_no][i];
         // increment for the benefit of the "anwer_"+i elements
         i++;
         // Set text, bg and function of each button
         document.getElementById("answer_"+i).textContent = quiz_data[question_no][index];
         document.getElementById("answer_"+i).style.background = "";
         document.getElementById("answer_"+i).onclick = incorrect;
+        // If question has already been iccorectly answered, highlight incorrect button selected
+        if(quiz_data[question_no][5] == true && i == answer_order[question_no][4]){
+            document.getElementById("answer_"+i).style.background = "red";
+        }
         // Answer should always be the first element in the csv
         if(index == 1){
             document.getElementById("answer_"+i).onclick = correct;
+            // If question has already been anwered then show correct answer
+            if(quiz_data[question_no][5] == true){
+                document.getElementById("answer_"+i).style.background = "green";
+            }
         }
+        i--;
     }
 }
 function correct(){
+    // Change bg of correct button green
     let correct_index = parseInt(this.id.slice(-1));
-    document.getElementById("answer_"+correct_index).style.background = "green";
+    if(quiz_data[question_no][5] == false){
+        document.getElementById("answer_"+correct_index).style.background = "green";
+        correct_answers++;
+        // update array so script knows this question has already been answered
+        quiz_data[question_no][5] = true;
+        show_question();
+    }
 }
 function incorrect(){
-    let correct_index = parseInt(this.id.slice(-1));
-    document.getElementById("answer_"+correct_index).style.background = "red";
+    // Change bg of incorrect button red
+    let incorrect_index = parseInt(this.id.slice(-1));
+    if(quiz_data[question_no][5] == false){
+        document.getElementById("answer_"+incorrect_index).style.background = "red";
+        // Store index of incorrect button
+        answer_order[question_no][4] = incorrect_index;
+        // update array so script knows this question has already been answered
+        quiz_data[question_no][5] = true;
+        show_question();
+    }
 }
 function random_sort(array){
-    // index range used for looping over array elements
+    // Index range used for looping over array elements
     let index = array.length;
     while(index > 0){
         // Generate random index to swap elements with
